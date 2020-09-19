70c9d19ad466ca1f444974e057def0483b70dff8
vvvcom248 <vvvcom248@gmail.com>
2020-09-19 23:24:25 +0200

    Тут ES6

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function init() {
+    $.getJSON("goods.json", goodsOut)//читаем фаил goods.json
+}
+function goodsOut(data) {//вывод на страницу
+    console.log(data);
+    var out = "";
+    for (var key in data) {
+        /*
+        ------ES5
+        out += '<div class="cart">';
+        out += '<p class="name">' + data[key].name + '</p>';
+        out += '<img src="images/' + data[key].img + '" alt="pictures">';
+        out += '<div class="cost">' + data[key].cost + '</div>';
+        out += '<buuton class="add-to-cart">Купить</buuton>';
+        out += '</div>';
+        */
+        //------ES6
+        out += '<div class="cart">';
+        out += `<p class="name">${data[key].name}</p>`;
+        out += `<img src="images/${data[key].img}" alt="pictures">`;
+        out += `<div class="cost">${data[key].cost}</div>`;
+        out += '<buuton class="add-to-cart">Купить</buuton>';
+        out += '</div>';
+
+    }
+    $('.goods-out').html(out);
+}
+init();
+$(document).ready(function () {
+
+})
